a75423b16233a6818e4e8b795319f819c7474443
Lukem121 <lukeask@hotmail.co.uk>
2021-03-04 23:20:52 +0000

    dd

--- src/components/Register.svelte
+++ src/components/Register.svelte
@@ -2,21 +2,35 @@
 	import Button from './Button.svelte';
 	import { slide } from 'svelte/transition';
     import { ethStore, chainId, web3, selectedAccount, connected } from 'svelte-web3';
+	import { abi, address } from '../_honeyToken.js';
+
 
     // Register
 	let joinHoneyPrice = 0.01;
 	let registerUsername = "";
 	let registerReferalCode = "";
+    let referralCodeExsists = false;
 
     //Loaders
 	let registerLoading = false;
 
+    // Set if refural code exsists
+    const setReferralCodeExsists = async () => {
+        referralCodeExsists = await getReferralCodeExsists();
+    }
 
+    // Check referral code exsists while typing
+    const getReferralCodeExsists = async(e) => {
+		let contract = new $web3.eth.Contract(abi, address);
+		return contract.methods.isAvailable(registerReferalCode).call().then(function(res) {
+			return res;
+		});
+	}
 
     const sendRegisterTransaction = async () => {
 		registerLoading = true;
 
-		//REMOVE THIS CODE BELOW
+		//REMOVE THIS CODE BELOW DEV VAR
 		registerd = true;
 
         let contract = new $web3.eth.Contract(abi, honeyContractAddress, { from: $selectedAccount });
@@ -34,10 +48,10 @@
 
 <form on:submit|preventDefault={sendRegisterTransaction} class="flex flex-col justify-center items-center">
     <div class="flex flex-col space-y-2 p-2 mb-1">
-        {#if registerReferalCode.length >= 3 }
+        {#if referralCodeExsists }
             <input transition:slide bind:value={registerUsername} required minLength="3" name="username" maxLength="18" class="rounded border w-56 shadow border-black px-4 py-1 text-lg" type="text" placeholder="Username">
         {/if}
-        <input bind:value={registerReferalCode} required minLength="3" maxLength="18" class="rounded border w-56 shadow text-yellow-400 placeholder-yellow-300 border-black  px-4 py-1 text-lg" type="text" placeholder="Referral Code">
+        <input bind:value={registerReferalCode} on:input={setReferralCodeExsists} required minLength="3" maxLength="18" class="rounded border w-56 shadow text-yellow-400 placeholder-yellow-300 border-black  px-4 py-1 text-lg" type="text" placeholder="Referral Code">
     </div>
     <Button on:click loading={registerLoading}>Register</Button>
 </form>
